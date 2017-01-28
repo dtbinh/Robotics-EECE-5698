5565905c3b1d46c3353c736adafa6ce8564afdd7
Alex Agudelo <awagud12@gmail.com>
2017-01-28 11:32:49 -0800

    Adding initial

--- homework2/gpsData.py
+++ homework2/gpsData.py
@@ -10,13 +10,15 @@ except ImportError:
 import struct
 
 class gpsData(object):
-    __slots__ = ["timestamp", "lat", "lon", "alt"]
+    __slots__ = ["timestamp", "lat", "lon", "alt", "utm_x", "utm_y"]
 
     def __init__(self):
         self.timestamp = 0.0
         self.lat = 0.0
         self.lon = 0.0
         self.alt = 0.0
+        self.utm_x = 0.0
+        self.utm_y = 0.0
 
     def encode(self):
         buf = BytesIO()
@@ -25,7 +27,7 @@ class gpsData(object):
         return buf.getvalue()
 
     def _encode_one(self, buf):
-        buf.write(struct.pack(">ffff", self.timestamp, self.lat, self.lon, self.alt))
+        buf.write(struct.pack(">ffffff", self.timestamp, self.lat, self.lon, self.alt, self.utm_x, self.utm_y))
 
     def decode(data):
         if hasattr(data, 'read'):
@@ -39,14 +41,14 @@ class gpsData(object):
 
     def _decode_one(buf):
         self = gpsData()
-        self.timestamp, self.lat, self.lon, self.alt = struct.unpack(">ffff", buf.read(16))
+        self.timestamp, self.lat, self.lon, self.alt, self.utm_x, self.utm_y = struct.unpack(">ffffff", buf.read(24))
         return self
     _decode_one = staticmethod(_decode_one)
 
     _hash = None
     def _get_hash_recursive(parents):
         if gpsData in parents: return 0
-        tmphash = (0x51ea26a4abc04a35) & 0xffffffffffffffff
+        tmphash = (0xa30f84a9c2cb5440) & 0xffffffffffffffff
         tmphash  = (((tmphash<<1)&0xffffffffffffffff)  + (tmphash>>63)) & 0xffffffffffffffff
         return tmphash
     _get_hash_recursive = staticmethod(_get_hash_recursive)
--- homework2/gpsNode.py
+++ homework2/gpsNode.py
@@ -7,7 +7,7 @@ import lcm
 import time
 import serial
 from exlcm import gpsData
-import csv
+import utm
 
 
 class Ct(object):
@@ -15,11 +15,41 @@ class Ct(object):
         self.port = serial.Serial(port_name, 4800, timeout=1.)  # 9600-N-8-1
         self.lcm = lcm.LCM("udpm://?ttl=12")
         self.gps_package = gpsData()
-    def msg_handler(self, data):
-        print("time stamp = %s" % str(data.timestamp))
-        print("latitude = %s" % str(data.lat))
-        print("longitude = %s" % str(data.lon))
-        print("Altitude = %s" % str(data.alt))
+    def msg_handler(self):
+        print("time stamp = %s" % str(self.gps_package.timestamp))
+        print("latitude = %s" % str(self.gps_package.lat))
+        print("longitude = %s" % str(self.gps_package.lon))
+        print("Altitude = %s" % str(self.gps_package.alt))
+        print("UTM_X = %s" % str(self.gps_package.utm_x))
+        print("UTM_Y %s" % str(self.gps_package.utm_y))
+
+    def SetUTMData(self):
+        utmData = utm.from_latlon(self.gps_package.lat, self.gps_package.lon)
+        self.gps_package.utm_x = utmData[0]
+        self.gps_package.utm_y = utmData[1]
+
+    def convertDMStoDec(self, data):
+        row = data.split(".")
+        hr = 0
+        mn = 0
+        sec = 0
+        # Need to test for 2 conditions
+        # Aka, 4 digits in front of . if lat, 5 if lon
+        if len(row[0]) == 4:
+            hr = int(row[0][0:2])
+            mn = int(row[0][2:4])
+            sec = int(row[1])
+        elif len(row[0]) == 5:
+            hr = int(row[0][0:3])
+            mn = int(row[0][3:5])
+            sec = int(row[1])
+        else:
+            print("Error, not recognized format")
+            return 0
+
+        dec = float(hr) + float(mn)/60 + float(sec)/3600
+        return dec
+
 
     def readloop(self):
         print("got into readloop")
@@ -28,26 +58,29 @@ class Ct(object):
             line = self.port.readline()
             line = f.readline()
             data = [x for x in line.split(',')]
-            try:
-                if data[0] == "$GPGGA":
-                    if(int(data[6]) != 0):
-                        self.gps_package.timestamp = float(data[1])
-                        self.gps_package.lat = float(data[2])
-                        self.gps_package.lon = float(data[4])
-                        fix = data[6]
-                        numSat = data[7]
-                        HDOP = data[8]
-                        self.gps_package.alt = float(data[9])
-                        heightGeoid = data[10]
-                        lastDGPS = data[11]
-                        DGPSref = data[12]
-                        self.msg_handler(self.gps_package)
-                        self.lcm.publish("Example", self.gps_package.encode())
-                        time.sleep(1)
-                    else:
-                        print("No GPS Signal")
-            except:
-                    print("Formating issues")
+#            try:
+            if data[0] == "$GPGGA":
+                if(int(data[6]) != 0):
+                    self.gps_package.timestamp = float(data[1])
+                    latDec = self.convertDMStoDec(data[2])
+                    self.gps_package.lat = latDec
+                    lonDec = self.convertDMStoDec(data[4])
+                    self.gps_package.lon = lonDec
+                    fix = data[6]
+                    numSat = data[7]
+                    HDOP = data[8]
+                    self.gps_package.alt = float(data[9])
+                    heightGeoid = data[10]
+                    lastDGPS = data[11]
+                    DGPSref = data[12]
+                    self.SetUTMData()
+                    self.msg_handler()
+                    self.lcm.publish("Example", self.gps_package.encode())
+                    time.sleep(1)
+                else:
+                    print("No GPS Signal")
+#           except:
+  #                  print("Formating issues")
                        
 
        
