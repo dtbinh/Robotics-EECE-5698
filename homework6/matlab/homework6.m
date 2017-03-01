7fd66dbe921c3307b2ab283136bac45c20033732
Alex Agudelo <awagud12@gmail.com>
2017-02-28 20:01:43 -0500

    Adding working code

--- homework6/matlab/homework6.m
+++ homework6/matlab/homework6.m
@@ -20,10 +20,10 @@ montage(buildingScene.Files)
 I = readimage(buildingScene, 1);
 
 % Initialize features for I(1)
-%grayImage = rgb2gray(I);
-points = detectSURFFeatures(I)
-[y,x,m] = harris(I, 1000, 'tile', [2 2], 'disp');
-features = [y,x,m];
+grayImage = rgb2gray(I);
+[y,x,m] = harris(grayImage, 1000, 'tile', [2 2], 'disp');
+%[y,x,features] = harris(I, 1000, 'tile', [3 3], 'disp');
+[features, points] = extractFeatures(grayImage,[x y]);
 
 % Initialize all the transforms to the identity matrix. Note that the
 % projective transform is used here because the building images are fairly
@@ -43,11 +43,11 @@ for n = 2:numImages
     I = readimage(buildingScene, n);
 
     % Detect and extract SURF features for I(n).
-    %grayImage = rgb2gray(I);
-    points = detectSURFFeatures(I)
-    [y,x,m] = harris(I, 1000, 'tile', [2 2], 'disp');
-    features = [y,x,m]
-
+    grayImage = rgb2gray(I);
+    [y,x,m] = harris(grayImage, 1000, 'tile', [2 2], 'disp');
+    %[y,x,features] = harris(I, 1000, 'tile', [3 3], 'disp');
+    [features, points] = extractFeatures(grayImage,[x y]);
+    
     % Find correspondences between I(n) and I(n-1).
     indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
 
@@ -61,3 +61,80 @@ for n = 2:numImages
     % Compute T(1) * ... * T(n-1) * T(n)
     tforms(n).T = tforms(n-1).T * tforms(n).T;
 end
+
+%% Output Limits
+
+imageSize = size(I);  % all the images are the same size
+
+% Compute the output limits  for each transform
+for i = 1:numel(tforms)
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
+end
+
+%% Set X Limit
+
+avgXLim = mean(xlim, 2);
+
+[~, idx] = sort(avgXLim);
+
+centerIdx = floor((numel(tforms)+1)/2);
+
+centerImageIdx = idx(centerIdx);
+
+%% Apply Center Image Inverse
+
+Tinv = invert(tforms(centerImageIdx));
+
+for i = 1:numel(tforms)
+    tforms(i).T = Tinv.T * tforms(i).T;
+end
+
+centerImageIdx = idx(centerIdx);
+
+%% Initalize Panorama
+
+for i = 1:numel(tforms)
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
+end
+
+% Find the minimum and maximum output limits
+xMin = min([1; xlim(:)]);
+xMax = max([imageSize(2); xlim(:)]);
+
+yMin = min([1; ylim(:)]);
+yMax = max([imageSize(1); ylim(:)]);
+
+% Width and height of panorama.
+width  = round(xMax - xMin);
+height = round(yMax - yMin);
+
+% Initialize the "empty" panorama.
+panorama = zeros([height width 3], 'like', I);
+
+%% Create the panorama
+
+blender = vision.AlphaBlender('Operation', 'Binary mask', ...
+    'MaskSource', 'Input port');
+
+% Create a 2-D spatial reference object defining the size of the panorama.
+xLimits = [xMin xMax];
+yLimits = [yMin yMax];
+panoramaView = imref2d([height width], xLimits, yLimits);
+
+% Create the panorama.
+for i = 1:numImages
+
+    I = readimage(buildingScene, i);
+
+    % Transform I into the panorama.
+    warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+
+    % Generate a binary mask.
+    mask = imwarp(true(size(I,1),size(I,2)), tforms(i), 'OutputView', panoramaView);
+
+    % Overlay the warpedImage onto the panorama.
+    panorama = step(blender, panorama, warpedImage, mask);
+end
+
+figure
+imshow(panorama)
